d8b573c2cd69a4503ff300cce25865b408ab7f55
Harrison Chapman <hchapman@bowdoin.edu>
2010-05-04 09:47:18 -0400

    pretty damn sure I got it down now

--- src/re330/Curve.cpp
+++ src/re330/Curve.cpp
@@ -5,18 +5,33 @@ using namespace std;
 using namespace RE330;
 
 const Vector3 &CubicCurve::interpolate_point(const float t) const {
-    return ((p[0] * p[0][2] * ((1-t)*(1-t)*(1-t)) +
-             p[1] * p[1][2] * ((1-t)*(1-t)*t) +
-             p[2] * p[2][2] * ((1-t)*t*t) +
-             p[3] * p[3][2] * (t*t*t)) / 
-            (p[0][2] + p[1][2] + p[2][2] + p[3][2]));
+    Vector3 a = p[0]*-1 + p[1]*3 + p[2]*-3 + p[3];
+    Vector3 b = p[0]*3 + p[1]*-6 + p[2]*3;
+    Vector3 c = p[0]*-3 + p[1]*3;
+    Vector3 d = p[0];
+
+    return (a *(t*t*t) + b *(t*t) + c *t + d);
+    // return ((p[0] * p[0][2] * ((1-t)*(1-t)*(1-t)) +
+    //          p[1] * p[1][2] * ((1-t)*(1-t)*t) +
+    //          p[2] * p[2][2] * ((1-t)*t*t) +
+    //          p[3] * p[3][2] * (t*t*t)) / 
+    //         (p[0][2] * ((1-t)*(1-t)*(1-t)) + 
+    //          p[1][2] * ((1-t)*(1-t)*t) +
+    //          p[2][2] * ((1-t)*t*t) +
+    //          p[3][2] * (t*t*t)));
 }
 const Vector3 &CubicCurve::interpolate_deriv(const float t) const {
-    return ((p[0] * p[0][2] * (-3*(1-t)*(1-t)) +
-             p[1] * p[1][2] * ((1-t)*(1-t) - 2*t*(1-t)) +
-             p[2] * p[2][2] * ((1-t)*t*2 - t*t) +
-             p[3] * p[3][2] * (3*t*t)) / 
-            (p[0][2] + p[1][2] + p[2][2] + p[3][2]));
+    Vector3 a = p[0]*-1 + p[1]*3 + p[2]*-3 + p[3];
+    Vector3 b = p[0]*3 + p[1]*-6 + p[2]*3;
+    Vector3 c = p[0]*-3 + p[1]*3;
+
+    return (a *(3*t*t) + b *(2*t) + c);
+
+    // return ((p[0] * p[0][2] * (-3*(1-t)*(1-t)) +
+    //          p[1] * p[1][2] * ((1-t)*(1-t) - 2*t*(1-t)) +
+    //          p[2] * p[2][2] * ((1-t)*t*2 - t*t) +
+    //          p[3] * p[3][2] * (3*t*t)) / 
+    //         (p[0][2] + p[1][2] + p[2][2] + p[3][2]));
 }
 
 const Vector3 &PiecewiseCurve::interpolate_point(const float t) const {
@@ -47,7 +62,7 @@ const Vector3 &PiecewiseCurve::interpolate_deriv(const float t) const {
     if (t >= 1) {
         return p[nPoints-1]-p[nPoints-2];
     } else if (t <= 0) {
-        return p[0]-p[1];
+        return p[1]-p[0];
     } else {
         // Determine which segment to interpolate
         int curve_i = (int)big_t;
